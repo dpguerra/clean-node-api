9db2453d6d463084614c383dfbb45a4c0e6b4223
Daniel Guerra <dpguerra@gmail.com>
2020-04-24 14:19:04 -0300

    feat: ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controllers/signUp.spec.ts
@@ -0,0 +1,16 @@
+import { SignUpController } from './signUp'
+describe('SignUp Controller', () => {
+  test('should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const request = {
+      body: {
+        name: '',
+        email: 'daniel@example.com',
+        password: 'password',
+        passwordConfirmation: 'password'
+      }
+    }
+    const response = sut.handle(request)
+    expect(response.statusCode).toBe(400)
+  })
+})
---
+++ src/presentation/controllers/signUp.ts
@@ -0,0 +1,11 @@
+interface SignUpBody {
+  body: { name: string, email: string, password: string, passwordConfirmation: string }
+}
+
+export class SignUpController {
+  handle (request: SignUpBody): any {
+    if (request.body.name === '') {
+      return { statusCode: 400 }
+    }
+  }
+}
