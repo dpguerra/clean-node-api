71ae4df00561624d11e2f7bb7f021a1758469cb5
Daniel Guerra <dpguerra@gmail.com>
2020-04-24 23:41:04 -0300

    feat: ensure SignUpController returns code 400 and an error if no password is provided

--- src/presentation/controllers/signUp.spec.ts
+++ src/presentation/controllers/signUp.spec.ts
@@ -30,4 +30,18 @@ describe('SignUp Controller', () => {
     expect(response.statusCode).toBe(400)
     expect(response.body).toEqual(new MissingParamError('email'))
   })
+  test('should return code 400 and a error if no password is provided', () => {
+    const sut = new SignUpController()
+    const request = {
+      body: {
+        name: 'Nome Qualquer',
+        email: 'nome@example.com',
+        password: '',
+        passwordConfirmation: 'password'
+      }
+    }
+    const response = sut.handle(request)
+    expect(response.statusCode).toBe(400)
+    expect(response.body).toEqual(new MissingParamError('password'))
+  })
 })
