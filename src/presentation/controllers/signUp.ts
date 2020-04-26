556ab978d41f6246613fb1e68bc9687d5015c83d
Daniel Guerra <dpguerra@gmail.com>
2020-04-26 00:56:24 -0300

    refactor: remove code duplication by destructoring request.body on SignUpController

--- src/presentation/controllers/signUp.ts
+++ src/presentation/controllers/signUp.ts
@@ -13,10 +13,11 @@ export class SignUpController implements Controller {
           return badRequest(new MissingParamError(field))
         }
       }
-      if (request.body.password !== request.body.passwordConfirmation) {
+      const { email, password, passwordConfirmation } = request.body
+      if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'))
       }
-      if (!this.emailValidator.isValid(request.body.email)) {
+      if (!this.emailValidator.isValid(email)) {
         return badRequest(new InvalidParamError('email'))
       }
       return { statusCode: 200 }
