58a26ca634515e7143594558b82b54283453839a
Daniel Guerra <dpguerra@gmail.com>
2020-04-28 14:54:45 -0300

    test: ensure AddAccountMongoRepository returns an account on success

--- src/infra/db/mongodb/accountRepository/account.spec.ts
+++ src/infra/db/mongodb/accountRepository/account.spec.ts
@@ -1,6 +1,20 @@
 import { AccountMongoRepository } from './account'
+import { MongoHelper } from '../helpers/mangoHelper'
+import { MongoMemoryServer } from 'mongodb-memory-server'
 
 describe('Account MongoDB Repository', () => {
+  const mongod = new MongoMemoryServer()
+
+  beforeAll(async () => {
+    const uri = await mongod.getUri()
+    await MongoHelper.connect(uri)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+    await mongod.stop()
+  })
+
   test('should returns an account on success', async () => {
     const sut = new AccountMongoRepository()
     const account = await sut.add({
