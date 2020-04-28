2f0e0e87509d9ad86796b338bd05985e03169e2f
Daniel Guerra <dpguerra@gmail.com>
2020-04-28 02:42:20 -0300

    test: ensure DbAddAccount returns account on success

--- src/data/usecases/addAccount/dbAddAccount.spec.ts
+++ src/data/usecases/addAccount/dbAddAccount.spec.ts
@@ -26,7 +26,7 @@ const makeAddAccountRepository = (): AddAccountRepository => {
         id: 'valid_id',
         name: 'valid_name',
         email: 'valid_email',
-        password: 'valid_password'
+        password: 'hashed_password'
       }
       return await Promise.resolve(fakeAccount)
     }
@@ -92,5 +92,20 @@ describe('DBAddAccount Usecase', () => {
     }
     const promise = sut.add(accountData)
     await expect(promise).rejects.toThrow()
+  })
+  test('should returns added account with correct values', async () => {
+    const { sut } = makeSut()
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    const account = await sut.add(accountData)
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password'
+    })
   })
 })
