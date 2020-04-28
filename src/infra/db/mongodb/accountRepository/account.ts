7ed9e4cfb6bfd4bc8a78003e7ea6348fb9007ad8
Daniel Guerra <dpguerra@gmail.com>
2020-04-28 14:53:20 -0300

    feat: ensure AddAccountMongoRepository returns an account on success

--- src/infra/db/mongodb/accountRepository/account.ts
+++ src/infra/db/mongodb/accountRepository/account.ts
@@ -1,14 +1,13 @@
 import { AddAccountRepository } from '../../../../data/protocols/addAccountRepository'
 import { AddAccountModel } from '../../../../domain/usecases/addAccount'
 import { AccountModel } from '../../../../domain/models/account'
+import { MongoHelper } from '../helpers/mangoHelper'
 
 export class AccountMongoRepository implements AddAccountRepository {
   async add (account: AddAccountModel): Promise<AccountModel> {
-    return await Promise.resolve({
-      id: 'any_id',
-      name: 'any_name',
-      email: 'any_email@exemple.com',
-      password: 'any_password'
-    })
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const { ops } = await accountCollection.insertOne(account)
+    const { _id, ...newAccount } = Object.assign({}, ops[0], { id: ops[0]._id })
+    return newAccount
   }
 }
