239ee2434ddc9eabc24f5af76311c31b045cd964
Daniel Guerra <dpguerra@gmail.com>
2020-04-29 16:57:57 -0300

    test: ensure LogControllerDecorator calls controller with correct values

---
+++ src/main/decorators/log.spec.ts
@@ -0,0 +1,65 @@
+import { LogControllerDecorator } from './log'
+import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
+
+interface SutTypes {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (request: HttpRequest): Promise<HttpResponse> {
+      const response = {
+        statusCode: 200,
+        body: {
+          name: 'any_name',
+          email: 'any_email',
+          password: 'any_password',
+          passwordConfirmation: 'any_password'
+        }
+      }
+      return await Promise.resolve(response)
+    }
+  }
+  return new ControllerStub()
+}
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeController()
+  return {
+    sut: new LogControllerDecorator(controllerStub),
+    controllerStub
+
+  }
+}
+
+describe('LogController Decorator', () => {
+  test('should controller handle calls original contoller with correcs values', async () => {
+    const { sut, controllerStub } = makeSut()
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+    const request = {
+      body: {
+        name: 'any_name',
+        email: 'any_email',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    await sut.handle(request)
+    expect(handleSpy).toHaveBeenCalledWith(request)
+  })
+  test('should controller handle', async () => {
+    const { sut, controllerStub } = makeSut()
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+    const request = {
+      body: {
+        name: 'any_name',
+        email: 'any_email',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    await sut.handle(request)
+    expect(handleSpy).toHaveBeenCalledWith(request)
+  })
+})
