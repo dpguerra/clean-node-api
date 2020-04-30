01863cbc7ae895c8554e1da74aa82e97d01231fb
Daniel Guerra <dpguerra@gmail.com>
2020-04-30 12:38:24 -0300

    refactor: move LogInController creation to factory

--- src/presentation/controllers/login/logIn.spec.ts
+++ src/presentation/controllers/login/logIn.spec.ts
@@ -3,9 +3,19 @@ import { HttpRequest, HttpResponse } from '../../protocols'
 import { badRequest } from '../../helpers'
 import { MissingParamError } from '../../errors'
 
+interface SutTypes {
+  sut: LogInController
+}
+
+const makeSup = (): SutTypes => {
+  return {
+    sut: new LogInController()
+  }
+}
+
 describe('LogIn Controller', () => {
   test('should returns 400 and an error if no email is passed', async () => {
-    const sut = new LogInController()
+    const { sut } = makeSup()
     const request: HttpRequest = {
       body: {
         password: 'valid_password'
@@ -15,7 +25,7 @@ describe('LogIn Controller', () => {
     expect(reponse).toEqual(badRequest(new MissingParamError('email')))
   })
   test('should returns 400 and an error if no password is passed', async () => {
-    const sut = new LogInController()
+    const { sut } = makeSup()
     const request: HttpRequest = {
       body: {
         email: 'valid_email@exemple.com'
