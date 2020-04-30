74601849b266059c7b8e53eb14eddc1a4a567774
Daniel Guerra <dpguerra@gmail.com>
2020-04-30 10:16:18 -0300

    test: ensure error log to be saved on correct repository

--- src/infra/db/mongodb/logErrorRepository/logError.spec.ts
+++ src/infra/db/mongodb/logErrorRepository/logError.spec.ts
@@ -1,14 +1,15 @@
-import { serverError } from '../../../../presentation/helpers'
 import { MongoHelper } from '../helpers/mongoHelper'
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import { LogErrorRepository } from '../../../../data/protocols/logErrorRepository'
 import { LogErrorMongoRepository } from './logError'
+import { Collection } from 'mongodb'
 
 const makeSut = (): LogErrorRepository => {
   return new LogErrorMongoRepository()
 }
 describe('Log Error MongoDB Repository', () => {
   const mongod = new MongoMemoryServer()
+  let errorCollection: Collection
 
   beforeAll(async () => {
     const uri = await mongod.getUri()
@@ -21,15 +22,18 @@ describe('Log Error MongoDB Repository', () => {
   })
 
   beforeEach(async () => {
-    const errorCollection = await MongoHelper.getCollection('errors')
+    errorCollection = await MongoHelper.getCollection('errors')
     await errorCollection.deleteMany({})
+  })
+  test('should create a database entry', async () => {
+    const sut = makeSut()
+    await sut.logError('any_stack')
+    const count = await errorCollection.countDocuments()
+    expect(count).toBe(1)
   })
   test('should returns un new id on success', async () => {
     const sut = makeSut()
-    const fakeError = new Error()
-    fakeError.stack = 'any_stack'
-    const error = serverError(fakeError)
-    const result = await sut.logError(error.body.stack)
+    const result = await sut.logError('any_stack')
     expect(result).toHaveProperty('id')
   })
 })
