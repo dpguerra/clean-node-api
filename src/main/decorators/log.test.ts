0bf24c806a1ad31d9b499a8398fb37fd65fffa28
Daniel Guerra <dpguerra@gmail.com>
2020-04-30 00:48:38 -0300

    test: ensure SignUpController returns un stackId if an error occurs

---
+++ src/main/decorators/log.test.ts
@@ -0,0 +1,66 @@
+import { EmailValidatorAdapter } from '../../utils/emailValidatorAdapater'
+import { BCryptAdapter } from '../../infra/criptography/bCryptAdapter'
+import { AccountMongoRepository } from '../../infra/db/mongodb/accountRepository/account'
+import { DbAddAccount } from '../../data/usecases/addAccount/dbAddAccount'
+import { SignUpController } from '../../presentation/controllers/signup/signUp'
+import { DBLogErrorRepository } from '../../infra/db/mongodb/logErrorRepository/logError'
+import { LogControllerDecorator } from './log'
+import { MongoMemoryServer } from 'mongodb-memory-server'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongoHelper'
+import { Controller } from '../../presentation/protocols'
+import { serverError } from '../../presentation/helpers'
+
+interface SutTypes {
+  sut: LogControllerDecorator
+  signUpController: Controller
+}
+
+const makeSut = (): SutTypes => {
+  const salt = 12
+  const emailValidatorAdapater = new EmailValidatorAdapter()
+  const bCryptAdapter = new BCryptAdapter(salt)
+  const accountMongoRepository = new AccountMongoRepository()
+  const dbAddAccount = new DbAddAccount(bCryptAdapter, accountMongoRepository)
+  const signUpController = new SignUpController(emailValidatorAdapater, dbAddAccount)
+  const dbLogErorRepository = new DBLogErrorRepository()
+  return {
+    sut: new LogControllerDecorator(signUpController, dbLogErorRepository),
+    signUpController
+  }
+}
+
+describe('Log Controller Decorator', () => {
+  const mongod = new MongoMemoryServer()
+
+  beforeAll(async () => {
+    const uri = await mongod.getUri()
+    await MongoHelper.connect(uri)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+    await mongod.stop()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+  test('should returns un new id on success', async () => {
+    const { sut, signUpController } = makeSut()
+    const fakeError = new Error()
+    fakeError.stack = 'any_stack'
+    const error = serverError(fakeError)
+    jest.spyOn(signUpController, 'handle').mockReturnValueOnce(Promise.resolve(error))
+    const request = {
+      body: {
+        name: 'any_name',
+        email: 'any_email',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const response = await sut.handle(request)
+    expect(response.body.stackId).toBeDefined()
+  })
+})
