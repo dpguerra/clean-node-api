ec51b8f2f954ebf60e7ba02a573cd86d93850d12
Daniel Guerra <dpguerra@gmail.com>
2020-05-02 17:54:56 -0300

    test: ensure MissingParamValidation helper return a error if required value has a empty value

--- src/presentation/helpers/validation/missingParamValidation.spec.ts
+++ src/presentation/helpers/validation/missingParamValidation.spec.ts
@@ -8,4 +8,12 @@ describe('Missing Params Validation Helper', () => {
     })
     expect(result).toEqual(new Error('required_field'))
   })
+  test('should return an error if required value is provided with empty value', () => {
+    const sut = new MissingParamValidation(['required_field'])
+    const result = sut.validate({
+      any_field: 'any_value',
+      required_field: ''
+    })
+    expect(result).toEqual(new Error('required_field'))
+  })
 })
