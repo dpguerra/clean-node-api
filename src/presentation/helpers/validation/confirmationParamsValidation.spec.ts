1a1a336c38dfa9b51c766c427198f682037df6c7
Daniel Guerra <dpguerra@gmail.com>
2020-05-02 20:08:24 -0300

    test: ensure ConfirmationParamsValidation helper returns null if values match

--- src/presentation/helpers/validation/confirmationParamsValidation.spec.ts
+++ src/presentation/helpers/validation/confirmationParamsValidation.spec.ts
@@ -1,12 +1,20 @@
 import { ConfirmationParamsValidation } from './confirmationParamsValidation'
 
 describe('Confirmation Params Validation Helper', () => {
-  test('should return an error if provided field are different', () => {
+  test('should returns an error if provided field are different', () => {
     const sut = new ConfirmationParamsValidation('password', 'passwordConfirmation')
     const result = sut.validate({
       password: 'correct_password',
       passwordConfirmation: 'wrong_password'
     })
     expect(result).toEqual(new Error('passwordConfirmation'))
+  })
+  test('should returns null if provided field match', () => {
+    const sut = new ConfirmationParamsValidation('password', 'passwordConfirmation')
+    const result = sut.validate({
+      password: 'correct_password',
+      passwordConfirmation: 'correct_password'
+    })
+    expect(result).toBeNull()
   })
 })
