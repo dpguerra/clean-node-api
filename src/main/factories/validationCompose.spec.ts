4b6a379d8cfd657cfdea806bc075046cd1533d23
Daniel Guerra <dpguerra@gmail.com>
2020-05-04 10:22:38 -0300

    refactor: move use of EmailValidatorAdapter to a fabric with stub classe

--- src/main/factories/validationCompose.spec.ts
+++ src/main/factories/validationCompose.spec.ts
@@ -1,17 +1,25 @@
 import { makeValidationCompose } from './validationCompose'
 import { ValidationCompose, RequiredFieldsValidation, ComparedFieldsValidation, EmailValidation } from '../../presentation/helpers/validation'
-import { EmailValidatorAdapter } from '../../utils/emailValidatorAdapater'
+import { EmailValidator } from '../../presentation/protocols/emailValidator'
 
 jest.mock('../../presentation/helpers/validation/validationCompose')
+
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  return new EmailValidatorStub()
+}
 
 describe('Validation Compose Factory', () => {
   test('should calls ValidationCompose helper with corrects values', () => {
     makeValidationCompose()
-    const emailValidatorAdapater = new EmailValidatorAdapter()
     expect(ValidationCompose).toHaveBeenCalledWith([
       new RequiredFieldsValidation(['name', 'email', 'password', 'passwordConfirmation']),
       new ComparedFieldsValidation('password', 'passwordConfirmation'),
-      new EmailValidation('email', emailValidatorAdapater)
+      new EmailValidation('email', makeEmailValidator())
     ])
   })
 })
