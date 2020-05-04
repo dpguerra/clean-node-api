ad52f67bcd7fb1b60352c873c7dc920158b01ab6
Daniel Guerra <dpguerra@gmail.com>
2020-05-04 09:22:53 -0300

    refactor: move ValidationCompose creation to a factory

--- src/main/factories/signUp.ts
+++ src/main/factories/signUp.ts
@@ -1,24 +1,18 @@
 import { SignUpController } from '../../presentation/controllers/signup/signUp'
-import { EmailValidatorAdapter } from '../../utils/emailValidatorAdapater'
 import { DbAddAccount } from '../../data/usecases/addAccount/dbAddAccount'
 import { BCryptAdapter } from '../../infra/criptography/bCryptAdapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/accountRepository/account'
 import { Controller } from '../../presentation/protocols/controller'
 import { LogControllerDecorator } from '../decorators/log'
 import { LogErrorMongoRepository } from '../../infra/db/mongodb/logErrorRepository/logError'
-import { ValidationCompose, RequiredFieldsValidation, ComparedFieldsValidation, EmailValidation } from '../../presentation/helpers/validation'
+import { makeValidationCompose } from './validationCompose'
 
 export const makeSignUpController = (): Controller => {
   const salt = 12
-  const emailValidatorAdapater = new EmailValidatorAdapter()
   const bCryptAdapter = new BCryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bCryptAdapter, accountMongoRepository)
-  const validation = new ValidationCompose([
-    new RequiredFieldsValidation(['name', 'email', 'password', 'passwordConfirmation']),
-    new ComparedFieldsValidation('password', 'passwordConfirmation'),
-    new EmailValidation(emailValidatorAdapater)
-  ])
+  const validation = makeValidationCompose()
   const signUpController = new SignUpController(dbAddAccount, validation)
   const LogErorMongoRepository = new LogErrorMongoRepository()
   return new LogControllerDecorator(signUpController, LogErorMongoRepository)
---
+++ src/main/factories/validationCompose.ts
@@ -0,0 +1,11 @@
+import { ValidationCompose, RequiredFieldsValidation, ComparedFieldsValidation, EmailValidation } from '../../presentation/helpers/validation'
+import { EmailValidatorAdapter } from '../../utils/emailValidatorAdapater'
+
+export const makeValidationCompose = (): ValidationCompose => {
+  const emailValidatorAdapater = new EmailValidatorAdapter()
+  return new ValidationCompose([
+    new RequiredFieldsValidation(['name', 'email', 'password', 'passwordConfirmation']),
+    new ComparedFieldsValidation('password', 'passwordConfirmation'),
+    new EmailValidation(emailValidatorAdapater)
+  ])
+}
