0df0f3331f9366754b22678a4e7591bbad7e3216
Daniel Guerra <dpguerra@gmail.com>
2020-05-05 00:55:09 -0300

    test: ensure DBAuthenticate calls LoadAccountByIdRepository whit correct value

---
+++ src/data/usecases/authentication/dbAuthenticate.spec.ts
@@ -0,0 +1,46 @@
+import { DBAuthenticate } from './dbAuthenticate'
+import { AccountModel } from '../../../domain/models/account'
+import { LoadAccountByIdRepository } from '../../protocols/db/loadAccountByIdRepository'
+import { Authentication, AuthenticationModel } from '../../../domain/usecases/authentication'
+
+const makeFakeAccount = (): AccountModel => ({
+  id: 'any_id',
+  name: 'any_name',
+  email: 'any_email',
+  password: 'any_hashed_password'
+})
+const makeFakeCredential = (): AuthenticationModel => ({
+  email: 'any_email',
+  password: 'any_password'
+})
+
+const makeLoadAccountByIdRepository = (): LoadAccountByIdRepository => {
+  class LoadAccountByIdRepositoryStub implements LoadAccountByIdRepository {
+    async load (email: string): Promise<AccountModel> {
+      return await Promise.resolve(makeFakeAccount())
+    }
+  }
+  return new LoadAccountByIdRepositoryStub()
+}
+
+interface SutTypes {
+  sut: Authentication
+  loadAccountByIdRepositoryStub: LoadAccountByIdRepository
+}
+const makeSut = (): SutTypes => {
+  const loadAccountByIdRepositoryStub = makeLoadAccountByIdRepository()
+  return {
+    sut: new DBAuthenticate(loadAccountByIdRepositoryStub),
+    loadAccountByIdRepositoryStub
+  }
+}
+
+describe('DBAuthenticate Usecase', () => {
+  test('should call LoadAccountByIdRepository with correct value', async () => {
+    const { sut, loadAccountByIdRepositoryStub } = makeSut()
+    const loadSpy = jest.spyOn(loadAccountByIdRepositoryStub, 'load')
+    const credential = makeFakeCredential()
+    await sut.auth(credential)
+    expect(loadSpy).toBeCalledWith(credential.email)
+  })
+})
