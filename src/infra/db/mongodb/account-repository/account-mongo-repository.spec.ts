845b68a3046085f2c9cd03d49b40ba596cbc755c
Daniel Guerra <dpguerra@gmail.com>
2020-05-05 18:53:20 -0300

    test: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/account-repository/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account-repository/account-mongo-repository.spec.ts
@@ -1,10 +1,12 @@
 import { AccountMongoRepository } from './account-mongo-repository'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { MongoMemoryServer } from 'mongodb-memory-server'
-import { AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
+import { AddAccountModel } from '../../../../domain/usecases/account'
+import { Collection } from 'mongodb'
 
 describe('Account MongoDB Repository', () => {
   const mongod = new MongoMemoryServer()
+  let accountCollection: Collection
 
   beforeAll(async () => {
     const uri = await mongod.getUri()
@@ -17,25 +19,36 @@ describe('Account MongoDB Repository', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
-  const makeSut = (): AddAccountRepository => {
+  const makeSut = (): AccountMongoRepository => {
     return new AccountMongoRepository()
   }
+  const makeFakeAccount = (): AddAccountModel => ({
+    name: 'any_name',
+    email: 'any_email@exemple.com',
+    password: 'any_password'
+  })
 
-  test('should returns an account on success', async () => {
-    const sut = makeSut()
-    const account = await sut.add({
-      name: 'any_name',
-      email: 'any_email@exemple.com',
-      password: 'any_password'
+  describe('Add Method', () => {
+    test('should returns an account on success', async () => {
+      const sut = makeSut()
+      const account = await sut.add(makeFakeAccount())
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@exemple.com')
+      expect(account.password).toBe('any_password')
     })
-    expect(account).toBeTruthy()
-    expect(account.id).toBeTruthy()
-    expect(account.name).toBe('any_name')
-    expect(account.email).toBe('any_email@exemple.com')
-    expect(account.password).toBe('any_password')
+  })
+  describe('LoadByEmail Method', () => {
+    test('should returns an account on success', async () => {
+      await accountCollection.insertOne(makeFakeAccount())
+      const sut = makeSut()
+      const account = await sut.loadByEmail('any_email@exemple.com')
+      expect(account).toBeTruthy()
+    })
   })
 })
