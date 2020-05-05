89461640fa91f748851bde1568ac85152553c9db
Daniel Guerra <dpguerra@gmail.com>
2020-05-05 12:13:52 -0300

    feat: ensure DBAuthenticate call Encrypter with corrects values

--- src/data/usecases/authentication/dbAuthenticate.ts
+++ src/data/usecases/authentication/dbAuthenticate.ts
@@ -1,11 +1,13 @@
 import { Authentication, AuthenticationModel } from '../../../domain/usecases/authentication'
 import { LoadAccountByIdRepository } from '../../protocols/db/loadAccountByIdRepository'
 import { HashComparer } from '../../protocols/criptography/hashComparer'
+import { Encrypter } from '../../protocols/criptography/encrypter'
 
 export class DBAuthenticate implements Authentication {
   constructor (
     private readonly loadAccountByIdRepository: LoadAccountByIdRepository,
-    private readonly hashCompare: HashComparer
+    private readonly hashCompare: HashComparer,
+    private readonly encrypter: Encrypter
   ) { }
 
   async auth (credential: AuthenticationModel): Promise<string> {
@@ -16,6 +18,7 @@ export class DBAuthenticate implements Authentication {
     if (!await this.hashCompare.compare(credential.password, account.password)) {
       return await Promise.reject(Error('unauthorized'))
     }
-    return await Promise.resolve('valid_token')
+    const token = await this.encrypter.encrypt(account.id)
+    return await Promise.resolve(token)
   }
 }
