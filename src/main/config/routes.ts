d46370650c46e2178ac944459b3dfb4a449907b9
Daniel Guerra <dpguerra@gmail.com>
2020-05-06 17:14:20 -0300

    refactor: change fast_glob to fs

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,8 +1,14 @@
 import { Express, Router } from 'express'
-import fastGlob from 'fast-glob'
+import { readdirSync } from 'fs'
+// import fastGlob from 'fast-glob'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  fastGlob.sync('**/main/routes/**.route.ts').map(async file => (await import(`../../../${file}`)).default(router))
+  readdirSync(`${__dirname}/../routes`).map(async file => {
+    if (!file.includes('.test.')) {
+      (await import(`../routes/${file}`)).default(router)
+    }
+  })
+  // fastGlob.sync('**/main/routes/**.route.ts').map(async file => (await import(`../../../${file}`)).default(router))
 }
