789311c06641d52e15fd2c4acd6e0230460bfe96
Daniel Guerra <dpguerra@gmail.com>
2020-05-06 17:45:06 -0300

    fix: some errors

--- src/main/routes/login.route.ts
+++ src/main/routes/login.route.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { adapterRoute } from '../adapters/express-route-adapter'
-import { makeSignUpController } from '../factories/signup-test/signup-factory'
+import { makeSignUpController } from '../factories/signup/signup-factory'
 import { makeLogInController } from '../factories/login/login-factory'
 
 export default (router: Router): void => {
