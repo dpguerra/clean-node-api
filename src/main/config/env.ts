633b22ff1b37042a3d94cd0821e907787f296df0
Daniel Guerra <dpguerra@gmail.com>
2020-05-06 17:17:47 -0300

    refactor: change external variable of mongo db

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGODB_URI ?? 'mongodb://localhost:27017/clean-node-api',
   port: process.env.NODE_PORT ?? 5050,
   jwtSecret: process.env.JWT_SECRET ?? '94B765F725AECEE93C12E7FB61791'
 }
