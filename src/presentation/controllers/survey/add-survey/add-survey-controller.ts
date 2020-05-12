858cc4c7c798b7c2dba3bfee0b3431562b38b0f1
Daniel Guerra <dpguerra@gmail.com>
2020-05-12 13:03:44 -0300

    feat: ensure AddSurveyController calls Validation with corrects values

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,15 +1,21 @@
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 import { AddSurveyRepository } from '../../../../data/protocols/db/add-survey-repository'
-import { serverError } from '../../../helpers'
+import { serverError, badRequest } from '../../../helpers'
+import { Validation } from '../../ident/login/login-controller-protocols'
 
 export class AddSurveyController implements Controller {
   constructor (
-    private readonly addSurvey: AddSurveyRepository
+    private readonly addSurvey: AddSurveyRepository,
+    private readonly validation: Validation<Error>
   ) { }
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
     try {
       const { body } = request
+      const validationResult = this.validation.validate(body)
+      if (validationResult) {
+        return badRequest(validationResult)
+      }
       await this.addSurvey.add(body)
       return await Promise.resolve({
         statusCode: 204
