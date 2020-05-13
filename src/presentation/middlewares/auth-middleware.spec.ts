e3e1f91335df22ab9000fc8ef05287aa0d6d2cf0
Daniel Guerra <dpguerra@gmail.com>
2020-05-13 12:30:08 -0300

    test: ensure AuthMiddleware calls LoadAccountByToken with correct value

---
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -0,0 +1,55 @@
+import { forbiden } from '../helpers'
+import { AccessDeniedError } from '../errors'
+import { Middleware } from '../protocols/middleware'
+import { AuthMiddleware } from './auth-middleware'
+import { HttpRequest } from '../protocols'
+import { LoadAccountByToken } from '../../domain/usecases/account/load-account-usecase'
+import { AccountModel } from '../../domain/models/account'
+
+const makeLoadAccountByToken = (): LoadAccountByToken => {
+  class LoadAccountByTokenStub implements LoadAccountByToken {
+    async load (token: string, role?: string): Promise<AccountModel | null> {
+      return await Promise.resolve(makeFakeAccount())
+    }
+  }
+  return new LoadAccountByTokenStub()
+}
+interface SutTypes {
+  sut: Middleware
+  loadAccountByTokenStub: LoadAccountByToken
+}
+
+const makeSut = (): SutTypes => {
+  const loadAccountByTokenStub = makeLoadAccountByToken()
+  return {
+    sut: new AuthMiddleware(loadAccountByTokenStub),
+    loadAccountByTokenStub
+  }
+}
+
+const makeFakeRequest = (): HttpRequest => ({
+  headers: {
+    'x-access-token': 'any_token'
+  }
+})
+const makeFakeAccount = (): AccountModel => ({
+  id: 'any_id',
+  name: 'any_name',
+  email: 'any_email',
+  password: 'any_password'
+})
+
+describe('Authentication Middleware tests', () => {
+  test('should return 403 if no x-access-token exists in headers', async () => {
+    const { sut } = makeSut()
+    const response = sut.handle({})
+    await expect(response).rejects.toEqual(forbiden(new AccessDeniedError()))
+  })
+  test('should call LoadAccountByToken with correct value', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut()
+    const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load')
+    const request = makeFakeRequest()
+    await sut.handle(request)
+    expect(loadSpy).toHaveBeenCalledWith(request.headers['x-access-token'])
+  })
+})
