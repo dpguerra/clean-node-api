3ec6323e4618eb5dee9bae17e3c3ecd5dd781a09
Daniel Guerra <dpguerra@gmail.com>
2020-05-14 10:41:15 -0300

    test: ensure DBLoadAccountByToken calls loadById on AccountMongoRepository with correct values

--- src/data/usecases/account/db-load-account-by-token.spec.ts
+++ src/data/usecases/account/db-load-account-by-token.spec.ts
@@ -1,5 +1,5 @@
-import { LoadAccountByToken, LoadAccountByTokenModel } from '../../../domain/usecases/account/load-account-usecase'
-import { LoadAccountByTokenRepository } from '../../protocols/db/load-account-by-token-repository'
+import { LoadAccountByToken, LoadAccountByTokenModel, LoadAccountByIdModel } from '../../../domain/usecases/account/load-account-usecase'
+import { LoadAccountByIdRepository } from '../../protocols/db/load-account-by-id-repository'
 import { AccountModel } from '../../../domain/models/account'
 import { DBLoadAccountByToken } from './db-load-account-by-token'
 import { Decrypter } from '../../protocols/criptography/decrypter'
@@ -12,9 +12,9 @@ const makeDecrypter = (): Decrypter => {
   }
   return new Descrypter()
 }
-const makeAccountMongoRepository = (): LoadAccountByTokenRepository => {
-  class AccountMongoRepositoryStub implements LoadAccountByTokenRepository {
-    async loadByToken (query: LoadAccountByTokenModel): Promise<AccountModel | null> {
+const makeAccountMongoRepository = (): LoadAccountByIdRepository => {
+  class AccountMongoRepositoryStub implements LoadAccountByIdRepository {
+    async loadById (query: LoadAccountByIdModel): Promise<AccountModel | null> {
       return await Promise.resolve(makeFakeAccount())
     }
   }
@@ -24,7 +24,7 @@ const makeAccountMongoRepository = (): LoadAccountByTokenRepository => {
 interface SutTypes {
   sut: LoadAccountByToken
   decrypterStub: Decrypter
-  accountMongoRepositoryStub: LoadAccountByTokenRepository
+  accountMongoRepositoryStub: LoadAccountByIdRepository
 }
 
 const makeSut = (): SutTypes => {
@@ -36,8 +36,12 @@ const makeSut = (): SutTypes => {
     accountMongoRepositoryStub
   }
 }
-const makeFakeQuery = (): LoadAccountByTokenModel => ({
+const makeFakeQueryToken = (): LoadAccountByTokenModel => ({
   token: 'any_token',
+  role: 'any_role'
+})
+const makeFakeQueryId = (): LoadAccountByIdModel => ({
+  id: 'any_id',
   role: 'any_role'
 })
 const makeFakeAccount = (): AccountModel => ({
@@ -51,34 +55,33 @@ describe('DBLoadAccountByToken Use Case tests', () => {
   test('should call Decrypter with correct value', async () => {
     const { sut, decrypterStub } = makeSut()
     const decryptSpy = jest.spyOn(decrypterStub, 'decrypt')
-    const query = makeFakeQuery()
+    const query = makeFakeQueryToken()
     await sut.load(query)
     expect(decryptSpy).toHaveBeenCalledWith(query.token)
   })
   test('should return null if Decryter returns null', async () => {
     const { sut, decrypterStub } = makeSut()
     jest.spyOn(decrypterStub, 'decrypt').mockResolvedValueOnce(null)
-    const account = await sut.load(makeFakeQuery())
+    const account = await sut.load(makeFakeQueryToken())
     expect(account).toBeNull()
   })
   test('should call AccountMongoRepository with correct values', async () => {
     const { sut, accountMongoRepositoryStub } = makeSut()
-    const loadByTokenSpy = jest.spyOn(accountMongoRepositoryStub, 'loadByToken')
-    const query = makeFakeQuery()
-    await sut.load(query)
-    expect(loadByTokenSpy).toHaveBeenCalledWith(query)
+    const loadByTokenSpy = jest.spyOn(accountMongoRepositoryStub, 'loadById')
+    await sut.load(makeFakeQueryToken())
+    expect(loadByTokenSpy).toHaveBeenCalledWith(makeFakeQueryId())
   })
   test('should throw if LoadAccountByTokenRepository throws', async () => {
     const { sut, accountMongoRepositoryStub } = makeSut()
-    jest.spyOn(accountMongoRepositoryStub, 'loadByToken').mockImplementationOnce(() => {
+    jest.spyOn(accountMongoRepositoryStub, 'loadById').mockImplementationOnce(() => {
       throw new Error()
     })
-    const promise = sut.load(makeFakeQuery())
+    const promise = sut.load(makeFakeQueryToken())
     await expect(promise).rejects.toThrow()
   })
   test('should return an account on success', async () => {
     const { sut } = makeSut()
-    const account = await sut.load(makeFakeQuery())
+    const account = await sut.load(makeFakeQueryToken())
     expect(account).toEqual(makeFakeAccount())
   })
 })
