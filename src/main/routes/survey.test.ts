f064e64b981032bac874a914dcdcb68fa91ad2df
Daniel Guerra <dpguerra@gmail.com>
2020-05-14 17:34:33 -0300

    test: ensure add survey route returns 403 if token provided is valid

--- src/main/routes/survey.test.ts
+++ src/main/routes/survey.test.ts
@@ -3,15 +3,20 @@ import app from '../config/app'
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import { Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
 
 describe('Survey Routes', () => {
   const mongod = new MongoMemoryServer()
   let surveyCollection: Collection
+  let accountCollection: Collection
 
   beforeAll(async () => {
     const uri = await mongod.getUri()
     await MongoHelper.connect(uri)
     surveyCollection = await MongoHelper.getCollection('surveys')
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.createIndex({ email: 1 }, { unique: true })
   })
 
   afterAll(async () => {
@@ -21,6 +26,7 @@ describe('Survey Routes', () => {
 
   beforeEach(async () => {
     await surveyCollection.deleteMany({})
+    await accountCollection.deleteMany({})
   })
   describe('POST /survey/add', () => {
     test('should return 403 without token', async () => {
@@ -45,6 +51,31 @@ describe('Survey Routes', () => {
           ]
         })
         .expect(403)
+    })
+    test('should return 204 with a valid token', async () => {
+      const { ops } = await accountCollection.insertOne({
+        name: 'valid_name',
+        email: 'valid_email@exemple.com',
+        password: 'any_password'
+      })
+      const id = ops[0]._id
+      const token = sign({ id }, env.jwtSecret)
+      console.log(token)
+      await accountCollection.updateOne({
+        _id: id
+      }, {
+        $set: { token }
+      })
+      await request(app)
+        .post('/api/survey/add')
+        .set('x-access-token', token)
+        .send({
+          question: 'valid_question',
+          answers: [
+            'valid_answer'
+          ]
+        })
+        .expect(204)
     })
   })
 })
